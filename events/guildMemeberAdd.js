25aed2444f48268dd010ebf911970f79b0c8429a
linulas <linubran90@gmail.com>
2019-11-10 20:39:44 +0000

    initial commit

---
+++ events/guildMemeberAdd.js
@@ -0,0 +1,7 @@
+module.exports = (client, member) => {
+    member.send(
+        `Welcome to The Pack! Here's some useful info to get you started:\n\n
+        Help your fellow Packmens to add you on various platforms: \n\n
+        Bot Commands`
+      )
+  }

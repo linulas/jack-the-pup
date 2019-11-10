ad3b08e59e95c741319a9ba49147cca0385786dd
Linus Brännström <linusbrannstrom@Linus-Brannstrom-Knowit-Experience-Norrland-AB.local>
2019-11-10 21:56:19 +0100

    fixed typo in filename

--- events/guildMemeberAdd.js
+++
@@ -1,7 +0,0 @@
-module.exports = (client, member) => {
-    member.send(
-        `Welcome to The Pack! Here's some useful info to get you started:\n\n
-        Help your fellow Packmens to add you on various platforms: \n\n
-        Bot Commands`
-      );
-  };
